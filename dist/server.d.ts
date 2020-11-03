b1200207c1bf180d5fde8e029f177043183fd64c
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-11-02 21:17:25 -0800

    fix

--- dist/server.d.ts
+++ dist/server.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="node" />
 import { IncomingMessage, ServerResponse, Server } from "http";
+import { Socket } from "net";
 declare type ReplyFunction = (msg: string | Buffer) => void;
 export interface WebSocketServerProps {
     onHttp?: (req: IncomingMessage, res: ServerResponse) => void;
-    onData?: (data: Buffer, reply: ReplyFunction) => void;
-    onConnection?: (reply: ReplyFunction) => void;
+    onData?: (data: Buffer, reply: ReplyFunction, session?: Record<string, any>, socket?: Socket) => void;
+    onConnection?: (reply: ReplyFunction, session?: Record<string, any>, socket?: Socket) => void;
     onListening?: () => void;
     port: number;
 }
